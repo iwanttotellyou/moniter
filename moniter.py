4b2a5e2ba2893a356b2a9ad6293a7db8289a7642
LionJun <592368998@qq.com>
2016-07-07 19:55:47 +0800

    ljun

--- moniter.py
+++ moniter.py
@@ -4,13 +4,21 @@ import commands
 app = Flask(__name__)
 
 
+def get_argv(maps, key, default=None):
+    if key in maps:
+        return maps[key]
+    if default is None:
+        return 0
+    return default
+
+
 @app.route('/', methods=['GET'])
 def main():
-    if "auth" in request.args and request.args['time'] == '20160606':
-        print commands.getoutput("git pull")
+    if get_argv(request.args, 'time') == '20160606':
+        print commands.getoutput("cd /root/blog-hugo && git pull")
         return '1'
     return '0'
 
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(port=8000)
